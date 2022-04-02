75cc077274c8b43f09508d14376cb164ba1288ac
Cameron Fathers <cameron.fathers@gmail.com>
2022-04-02 14:31:25 +1100

    Implemented interfaces for shared properties. Initial setup of 'Ship' class.

---
+++ BattleshipStateTracker/Interfaces/BoardStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleshipStateTracker.Interfaces
+{
+    interface BoardStatus
+    {
+        public string Unoccupied { get; set; }
+        public string Occupied { get; set; }
+        public string Hit { get; set; }
+        public string Miss { get; set; }
+    }
+}
---
+++ BattleshipStateTracker/Interfaces/ShipTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleshipStateTracker.Interfaces
+{
+    interface ShipTypes
+    {
+        public string AircraftCarrier { get; set; }
+        public string Battleship { get; set; }
+        public string Cruiser { get; set; }
+        public string Submarine { get; set; }
+        public string Destroyer { get; set; }
+    }
+}
